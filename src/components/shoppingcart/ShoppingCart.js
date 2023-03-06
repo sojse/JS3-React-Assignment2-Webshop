1bd585cb22cf99b2e4314e1d4556619af87e9607
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2023-03-06 16:02:22 +0100

    Add remove function to the context provider

--- src/components/shoppingcart/ShoppingCart.js
+++ src/components/shoppingcart/ShoppingCart.js
@@ -5,15 +5,6 @@ import './ShoppingCart.css';
 
 function ShoppingCart(props) {
 
-    /**
-     * Problem
-     * 
-     * Fastnar i oändliga loopar när jag försöker komma åt min context provider för att uppdatera min shopping cart
-     * 
-     * I Search resulten har jag en warning för mina dependencies, om jag lägger till den i dependency så blir det grönt i
-     * terminalen men jag fastnar i en oändlig loop
-     */
-    
     const shoppingCart = useContext(ShoppingCartContext);
     const [totalPrice, setTotal] = useState(0);
 
@@ -28,52 +19,17 @@ function ShoppingCart(props) {
             }
         }
 
-
         setTotal(newPrice);
-        
-        /*let updatedShoppingCart = [...shoppingCart.product];
-        let newPrice = 0;
-        let latestProduct = updatedShoppingCart[updatedShoppingCart.length - 1]
-
-        if(updatedShoppingCart.length > 0) {
-            updatedShoppingCart = updatedShoppingCart.filter((e, i, arr) => {
-                newPrice += e.product.price;
-
-                if(i !== updatedShoppingCart.length - 1 && e.product.productNumber === latestProduct.product.productNumber ) {
-                    //shoppingCart.setProducts(updatedShoppingCart);
-                    return false;
-                }
-                return true;
-            });
-        }
-        setTotal(newPrice);*/
 
     }, [shoppingCart]);
-
-    /**
-     * Filters through the array and removes the item with the matched id for the product that will be removed
-     */
-    let removeItem = (id) => {
-        let updatedShoppingCart = [];
-
-        updatedShoppingCart = shoppingCart.product.filter((e, i, arr) => {
-            if(e.product.productNumber === id) {
-                setTotal(totalPrice - e.product.price);
-                return false;
-            }
-            return true;
-        })
-
-        shoppingCart.setProducts(updatedShoppingCart);
-
-    }
+    
   
     return (
       <div className='shoppingCartContainer'>
         <h2>Shopping Cart</h2>
         <ul>
             {shoppingCart.product.map( (e, i) => {
-                return <ShoppingCartItem key={i} item={e} index={i} removeItem={removeItem} />
+                return <ShoppingCartItem key={i} item={e} index={i} removeItem={id => shoppingCart.removeFromCart(id)} />
             })}
         </ul>
         <div>
--- src/provider/ShoppingCartProvider.js
+++ src/provider/ShoppingCartProvider.js
@@ -3,26 +3,40 @@ import { useState } from "react";
 
 function ShoppingCartProvider({children}) {
     const [product, setProducts] = useState([]);
-    //{product: {mitt objekt}, quanity: 1}
 
-    let addToCart = (newItem) => {
+    /**
+     * adds a new item to the shopping cart, if there is already a item with the same product number the quantity will increase
+     */
+    const addToCart = (newItem) => {
         let updatedShoppingCart = [];
-        //console.log(product.quantity)
-        let newQuantity = 1;
+        let newQuantity = 1;    //setting the default quantity for when a product is added
         let latestProduct = newItem;
 
         updatedShoppingCart = product.filter((e, i, arr) => {
-            if(!isNaN(e.quantity)) { 
-                if(i !== arr.length && e.product.productNumber === latestProduct.productNumber) {
-                    newQuantity = e.quantity++;
-                    newQuantity++;
-                    return false;
-                }
+            // checks if the last added product is already in the shoppingcart, if it is the quantity will increase
+            if(i !== arr.length && e.product.productNumber === latestProduct.productNumber) {
+                newQuantity = e.quantity; // if the product is already in the shopping cart the previous value will be added
+                newQuantity++;
+                return false;
+            }
             return true;
-        }
         })
-        //console.log(newQuantity);
+
         updatedShoppingCart.push({product: newItem, quantity: newQuantity})
+        setProducts(updatedShoppingCart);
+    }
+
+    // filters through the array and removes the clicked product
+    const removeFromCart = (id) => {
+        let updatedShoppingCart = [];
+
+        updatedShoppingCart = product.filter((e, i, arr) => {
+            if(e.product.productNumber === id) {
+                return false;
+            }
+            return true;
+        })
+
         setProducts(updatedShoppingCart);
     }
 
@@ -30,8 +44,8 @@ function ShoppingCartProvider({children}) {
     return ( 
         <ShoppingCartContext.Provider value={{ 
                 product: product, 
-                setProducts: setProducts,
-                addToCart: addToCart
+                addToCart: addToCart,
+                removeFromCart: removeFromCart,
             }}>
             {children}
         </ShoppingCartContext.Provider>
